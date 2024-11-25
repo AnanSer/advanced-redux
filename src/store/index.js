56fe013f373cfc3c0602549575ea5524c715c269
AnanSer <ananserbesa@gmail.com>
2024-11-25 16:46:23 +0300

    added store using redux

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@testing-library/react";
+
+import uiSlice from "./ui-slice";
+
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
+
+export default store;
