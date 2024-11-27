5284253944a0433687e0ede99029dba92535c772
AnanSer <ananserbesa@gmail.com>
2024-11-27 19:58:09 +0300

    connected with database

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -5,7 +6,25 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://project-1-13247-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("sending cart data failed");
+      }
+    };
+  }, [cart]);
 
   return (
     <Layout>
